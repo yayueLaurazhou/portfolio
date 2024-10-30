9a8ec7d30c2a0d206856f3d436a795bd6cbd10f2
laura <yayuezhou2020@gmail.coom>
2024-10-30 22:20:36 +0800

    change skills img path

--- my-portfolio/public/data/skills.js
+++ my-portfolio/public/data/skills.js
@@ -1,22 +1,22 @@
 const skills = [
     {
         name: "Web Development",
-        icon: "public/img/web-development.svg",
+        icon: "/img/web-development.svg",
         description: "From UX/UI Design to Frontend Development, I can build responsive websites using Figma, React, Tailwind, Bootstrap, JavaScript. I am also familiar with Backend development using Python Django and Flask and PostgreSQL.",
     },
     {
         name: "Data Analysis and Visualization",
-        icon: "public/img/data-visualization.png",
+        icon: "/img/data-visualization.png",
         description: "I can use Python, Tableau and R to analyze and visualize data for better decision making.",
     },
     {
         name: "Data Engineering",
-        icon: "public/img/data-engineering.png",
+        icon: "/img/data-engineering.png",
         description: "I can design ETL(Extract, Transform, Load) Piplines using Python and AWS. Using Terraform to manipulate and manage infrastructure as code, GitHub Actions and Perfect to implement CI/CD, and Docker to containerize applications.",
     },
     {
         name: "Machine Learning",
-        icon: "public/img/machine-learning.png",
+        icon: "/img/machine-learning.png",
         description: "I have basic skills in using Pytorch to build LSTM and Transformer models for various NLP tasks such as summarization, classification and question answering",
     },
 ];
